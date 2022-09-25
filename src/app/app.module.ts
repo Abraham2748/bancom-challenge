c68c80c79d90e6fefdd0a338047c5e8d7c2bc6b9
Abraham Salazar <abraham_jack10@hotmail.com>
2022-09-25 08:56:50 -0500

    feat: Home page (Mobile)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,70 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home-mobile.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  lorem =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.';
+  features = [
+    {
+      img: '../../assets/home/feature-1.svg',
+      title: 'Robust workflow',
+      description: this.lorem,
+    },
+    {
+      img: '../../assets/home/feature-2.svg',
+      title: 'Flexibility',
+      description: this.lorem,
+    },
+    {
+      img: '../../assets/home/feature-3.svg',
+      title: 'User friendly',
+      description: this.lorem,
+    },
+    {
+      img: '../../assets/home/feature-4.svg',
+      title: 'Multiple layouts',
+      description: this.lorem,
+    },
+    {
+      img: '../../assets/home/feature-5.svg',
+      title: 'Better components',
+      description: this.lorem,
+    },
+    {
+      img: '../../assets/home/feature-6.svg',
+      title: 'Well organized',
+      description: this.lorem,
+    },
+  ];
 
-  constructor() { }
+  metrics = [
+    {
+      img: '../../assets/home/feature-1.svg',
+      title: '10,000+',
+      description: 'Downloads per day',
+    },
+    {
+      img: '../../assets/home/feature-4.svg',
+      title: '2 Million',
+      description: 'Users',
+    },
+    {
+      img: '../../assets/home/feature-5.svg',
+      title: '500+',
+      description: 'Clients',
+    },
+    {
+      img: '../../assets/home/feature-6.svg',
+      title: '140',
+      description: 'Countries',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  isMenuOpen = false;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
